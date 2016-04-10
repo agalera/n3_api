3cf0bb3eb41c806aa4ba1a2c5fbf5c5fd783dc79
Alberto Galera <galerajimenez@gmail.com>
2016-04-10 11:14:44 +0000

    PEP8

--- controllers/about.py
+++
@@ -1,9 +0,0 @@
-from bottle import get
-from modules.render import draw
-
-
-class About:
-
-    @get('/api/about')
-    def about(*args, **kwargs):
-        return draw(view='about', title="About")
